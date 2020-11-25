37e95de7dc25c173f2e1e4867d75a5de99e0e19c
Ege Ulgen <egeulgen@gmail.com>
2020-11-25 22:56:53 +0300

    proper data for test

--- tests/testthat/test-core_functions.R
+++ tests/testthat/test-core_functions.R
@@ -79,6 +79,8 @@ test_that("`create_features_df` works", {
     # batch analysis
     path2annovar_csv <- system.file("extdata/example_cohort.hg19_multianno.csv",
                                     package = "driveR")
+    path2phenolyzer_out <- system.file("extdata/example_cohort.annotated_gene_list",
+                                       package = "driveR")
     expect_is(features_df <- create_features_df(annovar_csv_path = path2annovar_csv,
                                                 scna_df = example_cohort_scna_table,
                                                 phenolyzer_annotated_gene_list_path = path2phenolyzer_out,
